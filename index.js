c24685368c01c0eeec297c10ba5717589547f61b
Shadowpig87 <Shadowpig87@gmail.com>
2024-11-26 19:45:57 -0500

    Add files via upload
    Signed-off-by: Shadowpig87 <Shadowpig87@gmail.com>

---
+++ index.js
@@ -0,0 +1,32 @@
+//variable = A container that stores value. 
+// behaves as if it were the value it contains.
+// 1. declaration let x;
+//2. assignment x=100;
+
+
+/*let firstname = "Bro";
+let grade = 95
+console.log(`grade`);
+console.log(`You grade is ${grade}`)
+console.log(`type of firstname`)
+console.log(`Your name is ${firstname}`)
+document.getElementById("myH1").textContent=`Hello World`;*/
+
+//window.alert(`My Application`)
+
+let online = false;
+let forsale = true;
+
+console.log(`Bro is online; ${online}`);
+console.log(`is this car for sale; ${true}`);
+
+let fullname = "Daniel Shapiro"
+let age = 13;
+let isStudent = true;
+let isCool = true;
+
+
+document.getElementById("p1").textContent = `My name is ${fullname}`;
+document.getElementById("p2").textContent = `My age is ${age}`;
+document.getElementById("p3").textContent = `Am I a student? ${isStudent}`;
+document.getElementById("p4").textContent = `Is Daniel Shapiro Cool? ${isCool}`;
